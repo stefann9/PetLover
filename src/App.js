3d93ad7fef131a6f0105f7704303fe31682e5496
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-13 16:10:23 +0300

    add router in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-// Add react-router-dom imports
-
-// create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(
+  createRoutesFromElements()
+);
 
 function App() {
   return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
