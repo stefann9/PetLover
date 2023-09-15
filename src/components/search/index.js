d51f072d1b9ada5b8e90fc2fb8001da7eb0e47b0
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-15 14:36:45 +0300

    add static filter in search comp and style

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,8 +1,7 @@
-import React, { useRef } from 'react';
-import { createSearchParams, useNavigate } from 'react-router-dom';
+import React, { useRef } from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
 
 const Search = () => {
-
   const navigate = useNavigate();
 
   const searchInputRef = useRef();
@@ -11,17 +10,29 @@ const Search = () => {
     e.preventDefault();
 
     const searchQuery = {
-      name: searchInputRef.current.value
-    }
-    const query = createSearchParams(searchQuery)
+      name: searchInputRef.current.value,
+    };
+    const query = createSearchParams(searchQuery);
     navigate({
-      pathname:"/search",
-      search:`?${query}`
-    })
+      pathname: "/search",
+      search: `?${query}`,
+    });
   };
 
   return (
     <form onSubmit={onSearchHandler} className="search-form">
+      <details className="filter">
+        <summary>Filter</summary>
+        <select id="pet-select">
+          <option value="">-- Choose a pet --</option>
+          <option value="dog">Dog</option>
+          <option value="cat">Cat</option>
+          <option value="hamster">Hamster</option>
+          <option value="parrot">Parrot</option>
+          <option value="spider">Spider</option>
+          <option value="goldfish">Goldfish</option>
+        </select>
+      </details>
       <input type="text" className="search" ref={searchInputRef} />
       <button type="submit" className="search-button">
         🔎
