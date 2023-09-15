d653ea45d35b30c57670cba0162286946f9a0cc5
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-09-15 16:56:16 +0300

    change searchSelectType from ref to state bc the comp needs to render onchange

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,20 +1,20 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 const Search = ({ petTypes }) => {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams()
-  const {type} = useParams();
+  const [searchParams] = useSearchParams();
+  const { type } = useParams();
 
   const searchInputRef = useRef();
-  const searchSelectType = useRef();
-
+  // const searchSelectType = useRef();
+  const [searchSelectType, setSearchSelectType] = useState("");
   const onSearchHandler = (e) => {
     e.preventDefault();
 
     const searchQuery = {
       name: searchInputRef.current.value,
-      type: searchSelectType.current.value || type || searchParams.get('type'),
+      type: searchSelectType || type || searchParams.get("type"),
     };
     const query = createSearchParams(searchQuery);
     navigate({
@@ -27,7 +27,7 @@ const Search = ({ petTypes }) => {
     <form onSubmit={onSearchHandler} className="search-form">
       <details className="filter">
         <summary>Filter</summary>
-        <select id="pet-select" ref={searchSelectType}>
+        <select id="pet-select" onChange={(e) => setSearchSelectType(e.target.value)}>
           <option value="">-- Choose a pet type --</option>
           {petTypes.map((type) => (
             <option key={type.name} value={type.name}>
